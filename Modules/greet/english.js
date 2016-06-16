82714a4bb1d6403503901cc2910bdd4f6d303d85
Walter <relw1wa-github@yahoo.de>
2016-06-16 09:12:48 +0200

    Learn NodeJS: use require with json data

--- Modules/greet/english.js
+++ Modules/greet/english.js
@@ -1,5 +1,8 @@
+// get json-file; node will convert json to javascript object
+var greetings = require('./greetings.json');
+
 var greet = function() {
-    console.log('Hello');
+    console.log(greetings.en);
 }
 
 module.exports = greet;
