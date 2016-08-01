855108230cd164ee8462510636fd886037df2ea1
Walter <relw1wa-github@yahoo.de>
2016-08-01 19:34:11 +0200

    Learn NodeJS: Implement routes that contain parameters

--- Express/app.js
+++ Express/app.js
@@ -5,13 +5,21 @@ var app  = express();
     on local machine use port 3000 */
 var port = process.env.PORT || 3000;
 
-// response to a GET request for '/'
+// respond to a GET request for '/'
 app.get('/', function(req, res) {
   res.send('<html><head></head><body><h1>Hello World</h1></body></html>');
 });
 
+/* respond to a GET request that contains a parameter
+    parameter is set in the url pattern via :id
+    parameter can be accessed via req.params.id */
+app.get('/person/:id', function(req, res) {
+  res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
+
+// respond to a GET request for /api
 app.get('/api', function(req, res) {
-  // respond with json data; express converts to json
+  // respond with json data; express automatically converts to json
   res.json({
     firstname: "John",
     lastname: "Doe"
