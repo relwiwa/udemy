0c099e85eee80df1d86643bfc234d3d2e8fd1773
Walter <relw1wa-github@yahoo.de>
2016-08-02 10:51:31 +0200

    Learn NodeJS: Obtain form and json POST-data via body-parser middleware

--- Express/app.js
+++ Express/app.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app  = express();
 
 /*  use port on production server via environmental variable;
     on local machine use port 3000 */
 var port = process.env.PORT || 3000;
+
+// use bodyParser to provide POST form and json data
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
+var jsonParser = bodyParser.json();
 
 /*  use ejs as template engine
     express uses views directory per default */
@@ -33,6 +40,22 @@ app.get('/person/:id', function(req, res) {
     ID: req.params.id,
     QSTR: req.query.srch
   });
+});
+
+/*  respond to POST request sending form data;
+    middleware urlencodedParser is run before the actual callback
+    function and provides the POST form data via req.body */
+app.post('/person', urlencodedParser, function(req, res) {
+  res.send('Thank you!');
+  console.log(req.body.firstname, req.body.lastname);
+});
+
+/*  respond to POST request sending json data;
+    middleware jsonParser is run before the actual callback
+    function and provides the POST json data via req.body */
+app.post('/person/json', jsonParser, function(req, res) {
+  res.send('Thank you for the JSON data!');
+  console.log(req.body.firstname, req.body.lastname);
 });
 
 // respond to a GET request for /api
