61ebc5b59385500042d668e80bf64457bf54a987
Walter <relw1wa-github@yahoo.de>
2016-11-23 13:00:16 +0100

    Angular2 + Node: Setup Frontend- Setup routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,18 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
+import { AuthenticationComponent } from "./auth/authentication.component";
+import { HeaderComponent } from './header.component';
 import { MessageComponent } from './messages/message.component';
 import { MessageInputComponent} from './messages/message-input.component';
 import { MessageListComponent } from './messages/message-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { routing } from './app.routing';
 
 @NgModule({
     declarations: [
         AppComponent,
+        AuthenticationComponent,
+        HeaderComponent,
         MessageComponent,
         MessageInputComponent,
-        MessageListComponent
+        MessageListComponent,
+        MessagesComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
