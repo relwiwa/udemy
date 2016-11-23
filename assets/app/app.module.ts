4c8061f834ba8cb3a09df6554a58c5d13d4510fe
Walter <relw1wa-github@yahoo.de>
2016-11-23 15:18:23 +0100

    Angular2 + Node: Setup Frontend- Setup child routes for signup, signin and logout

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -5,21 +5,27 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from './header.component';
+import { LogoutComponent } from './auth/logout.component';
 import { MessageComponent } from './messages/message.component';
 import { MessageInputComponent} from './messages/message-input.component';
 import { MessageListComponent } from './messages/message-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { routing } from './app.routing';
+import { SigninComponent } from './auth/signin.component';
+import { SignupComponent } from './auth/signup.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         AuthenticationComponent,
         HeaderComponent,
+        LogoutComponent,
         MessageComponent,
         MessageInputComponent,
         MessageListComponent,
-        MessagesComponent
+        MessagesComponent,
+        SigninComponent,
+        SignupComponent
     ],
     imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent]
