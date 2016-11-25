822214dfd5e3db4dd4e9d7161e3570e47cd1afe0
Walter <relw1wa-github@yahoo.de>
2016-11-25 09:50:21 +0100

    Angular2 + Node: Connect Front/Backend- Setup getting messages via get request

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -15,7 +15,7 @@ export class MessageService {
 
   addMessage(message: Message) {
     this.messages.push(message);
-    
+
     const body = JSON.stringify(message);
     const headers = {
       headers: new Headers({
@@ -32,7 +32,19 @@ export class MessageService {
   }
 
   getMessages() {
-    return this.messages;
+    return this.http.get('http://localhost:3000/message')
+    .map((response: Response) => {
+      const messages = response.json().obj;
+      let transformedMessages: Message[] = [];
+      for (let message of messages) {
+        transformedMessages.push(
+          new Message(message.content, 'Dummy', message.id, null)
+        );
+      }
+      this.messages = transformedMessages;
+      return transformedMessages;
+    })
+    .catch((error: Response) => Observable.throw(error.json()));
   }
 
   deleteMessage(message: Message) {
