7baf615aa25f13020a3cd6820c6eb3954327dd50
Walter <relw1wa-github@yahoo.de>
2016-11-25 10:55:12 +0100

    Angular2 + Node: Connect Front/Backend- Setup deleting messages via delete request

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -19,6 +19,10 @@ export class MessageComponent {
   }
 
   onDelete() {
-    this.messageService.deleteMessage(this.message);
+    this.messageService.deleteMessage(this.message)
+    .subscribe(
+      result => console.log(result),
+      error => console.error(error)
+    );
   }
 }
--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -69,10 +69,13 @@ export class MessageService {
       headers)
     .map((response: Response) => response.json())
     .catch((error: Response) => Observable.throw(error.json()));
-
   }
 
   deleteMessage(message: Message) {
+    // "optimistic update"
     this.messages.splice(this.messages.indexOf(message), 1);
+    return this.http.delete('http://localhost:3000/message/' + message.messageId)
+    .map((response: Response) => response.json())
+    .catch((error: Response) => Observable.throw(error.json()));
   }
 }
