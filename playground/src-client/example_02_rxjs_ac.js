cf14d9be3961084979521ec5aeb1edbeb8998924
Walter <relw1wa-github@yahoo.de>
2017-03-08 10:14:36 +0100

    Reactive Javascript: Use switchMap-function to getItems

--- playground/src-client/example_02_rxjs_ac.js
+++ playground/src-client/example_02_rxjs_ac.js
@@ -9,13 +9,16 @@ const keyUps$ = Rx.Observable.fromEvent($title, 'keyup')
 const queries$ = keyUps$
   .map(e => e.target.value)
   .distinctUntilChanged()
-  .debounceTime(250);
+  .debounceTime(250)
+  // - switchMap used to be called flatMapLatest;
+  // - returns only the latest result of ongoing queries
+  // - when there is new input to the observable, stops emitting previous value
+  //   and waits for result of latest input to be returned
+  .switchMap(query => getItems(query)); 
 
-queries$.subscribe(query => {
-  getItems(query).then(items => {
-    $results.empty();
-    $results.append(items.map(r => $(`<li />`).text(r)));
-  });
+queries$.subscribe(items => {
+  $results.empty();
+  $results.append(items.map(r => $(`<li />`).text(r)));
 });
 
 
