35de3bbc9b52f6008f0424edd2d05b8a80727dc3
Walter <relw1wa-github@yahoo.de>
2017-03-08 10:21:38 +0100

    Reactive Javascript: Create piped version of reactive autocomplete funtionality

--- playground/src-client/example_02_rxjs_ac.js
+++ playground/src-client/example_02_rxjs_ac.js
@@ -4,23 +4,15 @@ import Rx from 'rxjs/RX';
 const $title = $('#title');
 const $results = $('#results');
 
-// returns stream of keyup events
-const keyUps$ = Rx.Observable.fromEvent($title, 'keyup')
-const queries$ = keyUps$
+Rx.Observable.fromEvent($title, 'keyup')
   .map(e => e.target.value)
   .distinctUntilChanged()
-  .debounceTime(250)
-  // - switchMap used to be called flatMapLatest;
-  // - returns only the latest result of ongoing queries
-  // - when there is new input to the observable, stops emitting previous value
-  //   and waits for result of latest input to be returned
-  .switchMap(query => getItems(query)); 
-
-queries$.subscribe(items => {
-  $results.empty();
-  $results.append(items.map(r => $(`<li />`).text(r)));
-});
-
+  .debounceTime(500)
+  .switchMap(getItems)
+  .subscribe(items => {
+    $results.empty();
+    $results.append(items.map(r => $(`<li />`).text(r)));
+  });
 
 // API simulation
 function getItems(title) {
@@ -28,6 +20,6 @@ function getItems(title) {
   return new Promise((resolve, reject) => {
     window.setTimeout(() => {
       resolve([title, 'Item 2', 'Another ' + (Math.floor(Math.random()))]);
-    }, 500 + (Math.random() * 200));
+    }, 500 + (Math.random() * 2000));
   });
 }
