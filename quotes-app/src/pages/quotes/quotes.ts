12b3948327e8ba0f33a58c0ac4bcdb2d6f5df6c7
Walter <relw1wa-github@yahoo.de>
2017-03-11 20:35:10 +0100

    Ionic2: Quotes- Add alert to quotes page for add to favorites confirmation

--- quotes-app/src/pages/quotes/quotes.ts
+++ quotes-app/src/pages/quotes/quotes.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { NavParams } from 'ionic-angular';
+import { AlertController, NavParams } from 'ionic-angular';
 
 import { Quote } from '../../data/quote.interface';
 
@@ -12,7 +12,8 @@ import { Quote } from '../../data/quote.interface';
 export class QuotesPage implements OnInit {
   private quoteGroup: { category: string, quotes: Quote[], icon: string };
 
-  constructor(private navParams: NavParams) {}
+  constructor(private navParams: NavParams,
+              private alertCtrl: AlertController) {}
 
   /* In order to use ionViewDidLoad Hook, elvis operator (?) needs to be used in template,
      as template gets rendered by Angular (OnInit) before ionViewDidLoad hook starts */
@@ -22,6 +23,31 @@ export class QuotesPage implements OnInit {
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add this quote?',
+      buttons: [
+        /* role cancelled makes handler function execute also when user taps background outside of alert */
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('Canceled!');
+          }
+        }, {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('OK');
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
 }
