4440ec2a4a55f1327e8c68bae20864ea861535f4
Walter <relw1wa-github@yahoo.de>
2017-03-11 13:09:13 +0100

    Ionic2: Setup basic Ionic2 project

---
+++ ionic2-basics/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+// IonicModule already includes Angular's Http and Forms Module, so no need to import these extra
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  // Ionic Module wraps itself around MyApp to provide Ionic and Cordova functionality
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
