94c27a442bcac8fd4ac1ebd6ebdb49dc0eb239b6
Walter <relw1wa-github@yahoo.de>
2017-03-12 17:55:05 +0100

    React w/ Redux: Work on VideoList and VideoListItem components

--- ReduxSimpleStarter/src/components/video-list.js
+++ ReduxSimpleStarter/src/components/video-list.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import VideoListItem from './video-list-item';
 
 // 'props' contains data received from parent component via props passing
 const VideoList = (props) => {
+  // Arrays rendered as lists should have unique key attribute
+  // for enabling React to update changes effectively
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 }
