d11055017da387acf4d35381e8be17efa55b375e
Walter <relw1wa-github@yahoo.de>
2017-03-15 11:59:26 +0100

    React w/ Redux: Setup IndexRoute

--- redux-posts/src/routes.js
+++ redux-posts/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts-index';
 
 const Greeting = () => {
   return <div>Hey there!</div>;
@@ -9,6 +10,8 @@ const Greeting = () => {
 
 export default (
   <Route path="/" component={App}>
+    {/* IndexRoute matches, when none of the other child routes match */}
+    <IndexRoute component={PostsIndex} />
     <Route path="greet" component={Greeting} />
     <Route path="greet2" component={Greeting} />
     <Route path="greet3" component={Greeting} />
