df2fe49722dca5d36a17f47a616ecec8ef8d8085
Walter <relw1wa-github@yahoo.de>
2017-03-16 08:58:45 +0100

    React w/ Redux: Display list of blog posts in PostsIndex and use route params to navigate to individual PostsShow component

--- redux-posts/src/routes.js
+++ redux-posts/src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts-index';
 import PostsNew from './components/posts-new';
+import PostsShow from './components/posts-show';
 
 const Greeting = () => {
   return <div>Hey there!</div>;
@@ -13,6 +14,7 @@ export default (
   <Route path="/" component={App}>
     {/* IndexRoute matches, when none of the other child routes match */}
     <IndexRoute component={PostsIndex} />
-    <Route path="/posts/new" component={PostsNew} />
+    <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
