10387467fc71308e7185adcfb89e732b7f5bc75d
Walter <relw1wa-github@yahoo.de>
2017-03-19 10:35:51 +0100

    Webpack 2: Use code splitting via a System.import call

--- js-modules/src/image-viewer.js
+++ js-modules/src/image-viewer.js
@@ -1,11 +1,8 @@
 import '../styles/image-viewer.css';
-import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 
-const image1 = document.createElement('img');
-image1.src = small;
-document.body.appendChild(image1);
-
-const image2 = document.createElement('img');
-image2.src = big;
-document.body.appendChild(image2);
+export default () => {
+  const image1 = document.createElement('img');
+  image1.src = small;
+  document.body.appendChild(image1);
+}
--- js-modules/src/index.js
+++ js-modules/src/index.js
@@ -1,6 +1,10 @@
-import sum from './sum';
-import './image-viewer';
+const button = document.createElement('button');
+button.innerText = 'CLick me';
+button.onclick = () => {
+  System.import('./image-viewer')
+  .then(module => {
+    module.default();
+  });
+};
 
-const total = sum(10, 5);
-
-console.log(total);
+document.body.appendChild(button);
