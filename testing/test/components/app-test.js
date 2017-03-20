1c2700288783bc8474e0fb4fddb584ce08777e72
Walter <relw1wa-github@yahoo.de>
2017-03-20 11:37:18 +0100

    Advanced Redux: Testing- Add test for existence of child component via finding the respective css class

--- testing/test/components/app-test.js
+++ testing/test/components/app-test.js
@@ -3,15 +3,18 @@ import App from '../../src/components/app';
 
 // Use 'describe' to group together similar tests
 describe('App', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
 
   // Use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
-
-    // Create an instance of App
-    const component = renderComponent(App);
+  it('shows a comment box', () => {
 
     // Use 'expect' to make an assertion about a target
-    expect(component).to.contain('React simple starter');
+    // css-class of component is used to test existence of component
+    expect(component.find('.comment-box')).to.exist;
 
   });
 
