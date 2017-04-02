7c5afa0b7a7ad256769ca8a4cc750379494aa933
Walter <relw1wa-github@yahoo.de>
2017-04-02 10:14:07 +0200

    Advanced Redux: HOC- Wrap Resources route with Authentication higher order component

--- higher-order-components/src/components/hoc/require-authentication.js
+++ higher-order-components/src/components/hoc/require-authentication.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // ComposedComponent is component that will be wrapped by HOC
 export default function(ComposedComponent) {
@@ -9,5 +10,12 @@ export default function(ComposedComponent) {
       return <ComposedComponent {...this.props} />
     }
   }
-  return Authentication;
+
+  function mapStateToProps(state) {
+    return {
+      authenticated: state.authenticated
+    };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
