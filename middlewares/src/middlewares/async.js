b7f602457b502750e90de493580475586e8c124b
Walter <relw1wa-github@yahoo.de>
2017-04-02 12:34:50 +0200

    Advanced Redux: Middleware- Setup async middleware

--- middlewares/src/middlewares/async.js
+++ middlewares/src/middlewares/async.js
@@ -1,10 +1,22 @@
 export default function({ dispatch }) {
   // middleware chain
   return next => action => {
-    console.log(action);
+    // check for existence of promise (and payload)
+    if (!action.payload || !action.payload.then) {
+      // send result on to next middleware; if there's no more middleware,
+      // send it to reducers
+      return next(action);
+    }
 
-    // send result on to next middleware; if there's no more middleware,
-    // send it to reducers
-    next(action);
+    action.payload
+    .then((response) => {
+      // create new action with the old action type, but replace
+      // the promise with the response data as payload
+       const newAction = {...action, payload: response };
+       // instead of calling next, call dispatch to make sure all
+       // middlewares are called again with the response data
+       // => this is necessary whenever a action or payload is changed
+       dispatch(newAction);
+    });
   };
 }
