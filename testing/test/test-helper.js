9beb8f9c78363278dbd8da1b532444a79e8fdd4c
Walter <relw1wa-github@yahoo.de>
2017-08-20 11:00:31 +0200

    Advanced Redux: Testing- Setup chaiJquery

--- testing/test/test-helper.js
+++ testing/test/test-helper.js
@@ -2,11 +2,12 @@ import jsdom from 'jsdom';
 import _$ from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
 // 1. Set up testing environment to run like a browser in the command line
 
@@ -55,5 +56,10 @@ $.fn.simulate = function(eventName, value) {
     }
     TestUtils.Simulate[eventName](this[0]);
 }
+
+// 4. Set up chai-jquery
+
+chaiJquery(chai, chai.util, $);
+
 
 export { renderComponent, expect };
