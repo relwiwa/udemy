6fbbed00266cd8bcb396ff349f4729c036ec0f52
Walter <relw1wa-github@yahoo.de>
2017-08-20 10:50:33 +0200

    Advanced Redux: Testing- Setup renderComponent function

--- testing/test/test-helper.js
+++ testing/test/test-helper.js
@@ -1,5 +1,12 @@
 import jsdom from 'jsdom';
 import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
 // 1. Set up testing environment to run like a browser in the command line
 
@@ -11,3 +18,29 @@ global.window = global.document.defaultView;
 
 /*  - make jQuery Library's $ use global.window */ 
 const $ = _$(global.window);
+
+
+// 2. Build 'renderComponent helper that should render a given react class
+
+/*  renderComponent gets the following parameters:
+    - Respective React component class to be rendered
+    - props object of this component to be added to rendered Component
+    - state object with initial state configuration to be passed on to
+      Redux createStore function */
+function renderComponent(ComponentClass, props, state) {
+    /*  - TestUtils.renderIntoDocument renders a JSX Object into a DOM and
+          needs a DOM with access to window object
+        - DOM and window object were set up via JSDOM
+        - Redux Store Provider needs to be rendered as well */
+    const componentInstance = TestUtils.renderIntoDocument(
+        <Provider store={createStore(reducers, state)}>
+            <ComponentClass {...props} />
+        </Provider>
+    );
+
+    /*  - ReactDOM.findDOMNode returns actual HTML Element corresponding
+        - HTML element gets returned as jquerified */
+    return $(ReactDOM.findDOMNode(componentInstance));
+}
+
+export { renderComponent, expect };
