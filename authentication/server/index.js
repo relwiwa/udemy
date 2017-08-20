27541080d33056c53066169fe7b09124ae073e7f
Walter <relw1wa-github@yahoo.de>
2017-08-20 12:24:31 +0200

    Advanced Redux: Auth/Server- Setup Express

---
+++ authentication/server/index.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+
+const app = express();
+const router = require('./router');
+
+
+// App setup
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*.*' }));
+router(app);
+
+// Server setup
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on port ' + port);
