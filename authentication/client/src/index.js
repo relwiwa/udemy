ca4a0e132cc1b280c27184ec7f35c739a1e21a0d
Walter <relw1wa-github@yahoo.de>
2017-08-21 13:52:46 +0200

    Advanced Redux: Auth/Client- Check login-state before application startup

--- authentication/client/src/index.js
+++ authentication/client/src/index.js
@@ -13,11 +13,20 @@ import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import Welcome from './components/welcome';
+import { AUTH_USER } from './actions/types';
 
+/*  - Check if user is logged in (token exists)
+    - If so, update application state
+    - This happens, before store is added to React-Redux application via Provider */
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: AUTH_USER});
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Welcome} />
