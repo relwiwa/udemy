1441f507abe4a31b77aae5051b867612e619d3cf
Walter <relw1wa-github@yahoo.de>
2017-09-29 19:38:10 +0200

    React TDD and BDD: Giftgiver- Setup App component in TDD-way with snapshot test

---
+++ giftgiver/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Gift Giver</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ giftgiver/src/App.test.js
@@ -0,0 +1,10 @@
+import Adapter from './Enzyme.config';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+const app = shallow(<App />);
+
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot();
+});
