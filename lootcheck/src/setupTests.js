c7a27e374ee501b87afe7ef325be7eb85621b2e7
Walter <relw1wa-github@yahoo.de>
2017-09-30 11:59:02 +0200

    React TDD and BDD: Lootcheck- Add requestAnimationFrame shim for testing that React 16 requires for older browsers

--- lootcheck/src/setupTests.js
+++ lootcheck/src/setupTests.js
@@ -1,4 +1,8 @@
-
+/*  requestAnimationFrame shim for testing needs to be in separate file
+    inclusion via setupFiles in package.json Jest config doesn't work with
+    create-react-cli:
+    https://github.com/facebook/jest/issues/4545#issuecomment-332851105 */
+import reactShim from './react-shim';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
