d9928a5f575cec8bfe74fa596cf831249de02731
Walter <relw1wa-github@yahoo.de>
2017-09-30 12:48:04 +0200

    React TDD and BDD: Lootcheck- Setup redux store

--- lootcheck/src/index.js
+++ lootcheck/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducers/balance';
 import App from './components/App';
 
+const store = createStore(rootReducer);
+
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
