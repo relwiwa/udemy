3c08f0160274f25e4efa73c42e7418b9992097b0
Walter <relw1wa-github@yahoo.de>
2017-09-30 13:18:54 +0200

    React TDD and BDD: Lootcheck- Setup deposit and withdraw actions and reducers in a TDD way

--- lootcheck/src/actions/balance.test.js
+++ lootcheck/src/actions/balance.test.js
@@ -7,6 +7,23 @@ it('creates an action to set the balance', () => {
     type: constants.SET_BALANCE,
     balance
   };
-
   expect(actions.setBalance(balance)).toEqual(expectedAction);
+});
+
+it('creates an action to deposit into the balance', () => {
+  const deposit = 10;
+  const expectedAction = {
+    type: constants.DEPOSIT,
+    deposit
+  };
+  expect(actions.deposit(deposit)).toEqual(expectedAction);
+});
+
+it('creates an action to withdraw from the balance', () => {
+  const withdrawal = 10;
+  const expectedAction = {
+    type: constants.WITHDRAW,
+    withdrawal
+  };
+  expect(actions.withdraw(withdrawal)).toEqual(expectedAction);
 });
--- lootcheck/src/actions/constants.js
+++ lootcheck/src/actions/constants.js
@@ -1,1 +1,4 @@
+export const DEPOSIT = 'DEPOSIT';
 export const SET_BALANCE = 'SET_BALANCE';
+export const WITHDRAW = 'WITHDRAW';
+
