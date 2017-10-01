60df7510784184d0daa31fa0d6b3d80d21a54fc1
Walter <relw1wa-github@yahoo.de>
2017-10-01 20:17:14 +0200

    GraphQL With React: Section 8- Setup SongCreate component
    Use query variable to pass data from component to mutation outside of component

---
+++ lyrical/client/components/SongCreate.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: ''
+    };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    // pass data from within the component('s state) to the mutation
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create a New Song</h3>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title:</label>
+          <input
+            onChange={event => this.setState({ title: event.target.value })}
+            value={this.state.title}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+// use query variable so it can be passed from within the component
+const mutation = gql`
+  mutation AddSong($title: String){
+    addSong(
+      title: $title
+    ) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
