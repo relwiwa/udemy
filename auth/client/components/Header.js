c98aab68282b303d3b09d2c3456ba4811e5b1bd7
Walter <relw1wa-github@yahoo.de>
2017-10-14 21:27:17 +0200

    GraphQL With React: Section 13- Setup Header component along with Logout functionality

--- auth/client/components/Header.js
+++ auth/client/components/Header.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import { graphql } from 'react-apollo';
 
 import CurrentUserQuery from '../queries/CurrentUser';
+import LogoutMutation from '../mutations/Logout';
 
 class Header extends Component {
+
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query: CurrentUserQuery }]
+    });
+  }
+
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) { return <div /> }
+
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+        </li>
+      );
+    }
+    else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  }
+
   render() {
-    console.log(this.props.data);
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">Home</Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul> 
+        </div>
+      </nav>
     );  
   }
 }
 
-export default graphql(CurrentUserQuery)(Header);
+export default graphql(LogoutMutation)(
+  graphql(CurrentUserQuery)(Header)
+);
