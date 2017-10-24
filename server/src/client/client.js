998d912fce79a174d2640b53c40a8d2f40ea365c
Walter <relw1wa-github@yahoo.de>
2017-10-24 11:23:32 +0200

    SSR React/Redux: Redux- Setup FETCH_USERS action and reducer

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
+import reducers from './reducers';
 import Routes from './Routes';
 
 const store = createStore(
