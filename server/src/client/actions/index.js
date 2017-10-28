f3dc755952e02cbccfa3995635e22f8641e51dd7
Walter <relw1wa-github@yahoo.de>
2017-10-28 13:28:04 +0200

    SSR React/Redux: Auth- Setup admins action and reducer

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,12 +1,12 @@
-export const FETCH_USERS = 'fetch_users';
+export const FETCH_ADMINS = 'fetch_admins';
 export const FETCH_CURRENT_USER = 'fetch_current_user';
+export const FETCH_USERS = 'fetch_users';
 
-export const fetchUsers = () => async (dispatch, getState, axiosApiAccess) => {
-  // request is to api/users via Axios baseUrl
-  const res = await axiosApiAccess.get('/users');
+export const fetchAdmins = () => async (dispatch, getState, axiosApiAccess) => {
+  const res = await axiosApiAccess.get('/admins');
 
   dispatch({
-    type: FETCH_USERS,
+    type: FETCH_ADMINS,
     payload: res,
   });
 };
@@ -16,6 +16,16 @@ export const fetchCurrentUser = () => async (dispatch, getState, axiosApiAccess)
 
   dispatch({
     type: FETCH_CURRENT_USER,
+    payload: res,
+  });
+};
+
+export const fetchUsers = () => async (dispatch, getState, axiosApiAccess) => {
+  // request is to api/users via Axios baseUrl
+  const res = await axiosApiAccess.get('/users');
+
+  dispatch({
+    type: FETCH_USERS,
     payload: res,
   });
 };
--- server/src/client/reducers/index.js
+++ server/src/client/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
+import adminsReducer from './adminsReducer';
 import authReducer from './authReducer';
 import usersReducer from './usersReducer';
 
 export default combineReducers({
+  admins: adminsReducer,
   auth: authReducer,
   users: usersReducer,
 });
