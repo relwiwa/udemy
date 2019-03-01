4a050f626b2810a488cb55ddc37d5b7caa53075d
Walter <relw1wa-github@yahoo.de>
2019-03-01 20:48:34 +0100

    Add student: Setup jdbc code

--- dbdemo/src/com/relwiwa/web/jdbc/StudentDbUtil.java
+++ dbdemo/src/com/relwiwa/web/jdbc/StudentDbUtil.java
@@ -1,7 +1,9 @@
 package com.relwiwa.web.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -50,8 +52,34 @@ public class StudentDbUtil {
 		}
 	}
 	
-	public void addStudent(Student theStudent) {
+	public void addStudent(Student theStudent) throws SQLException {
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
 		
+		try {
+			// get db connection
+			myConn = dataSource.getConnection();
+
+			// create sql for insert
+			String sql = "insert into student "
+						+ "(first_name, last_name, email) "
+						+ "values (?, ?, ?)";
+			
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set the param values for the student
+			myStmt.setString(1,  theStudent.getFirstName());
+			myStmt.setString(2,  theStudent.getLastName());
+			myStmt.setString(3,  theStudent.getEmail());
+
+			// execute sql insert
+			myStmt.execute();
+		}
+		
+		finally {
+			// clean up jdbc objects
+			close(myConn, myStmt, null);
+		}
 	}
 	
 	private void close(Connection myConn, Statement myStmt, ResultSet myRs) {
