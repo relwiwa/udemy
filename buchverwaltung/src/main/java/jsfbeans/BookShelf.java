ad71e8349493015a15e7f42c862d5765a9c31e7b
Walter <relw1wa-github@yahoo.de>
2019-04-19 13:42:06 +0200

    Implement create book functionality

--- buchverwaltung/src/main/java/jsfbeans/BookShelf.java
+++ buchverwaltung/src/main/java/jsfbeans/BookShelf.java
@@ -14,44 +14,72 @@ import javax.faces.model.ListDataModel;
 
 public class BookShelf implements Serializable{
 
-           private static final long serialVersionUID = 1L;
-           private static final ArrayList<Book> books; 
+       private static final long serialVersionUID = 1L;
+       private static final ArrayList<Book> books; 
 
-           static{
-               books = new ArrayList<Book>();
-               Book book1 = new Book();
-               book1.setAuthor("Johann Wolfgang von Goethe");
-               book1.setName("Faust");
-               book1.setLanguage("de");
-               // Date beginnt bei 1900
-               book1.setPublishDate(new Date(1829 - 1900, 1, 19));
-               book1.setPrice(5.00);
-               books.add(book1);
-	
-               Book book2 = new Book();
-               book2.setAuthor("William Shakespeare");
-               book2.setName("Romeo und Julia");
-               book2.setLanguage("en");
-               book2.setPublishDate(new Date(1597 - 1900, 1, 1));
-               book2.setPrice(8.49);
-               books.add(book2);
+       static{
+           books = new ArrayList<Book>();
+           Book book1 = new Book();
+           book1.setAuthor("Johann Wolfgang von Goethe");
+           book1.setName("Faust");
+           book1.setLanguage("de");
+           // Date beginnt bei 1900
+           book1.setPublishDate(new Date(1829 - 1900, 1, 19));
+           book1.setPrice(5.00);
+           books.add(book1);
 
-               Book book3 = new Book();
-               book3.setAuthor("Stephen King");
-               book3.setName("Es - It");
-               book3.setLanguage("en");
-               book3.setPublishDate(new Date(1986 - 1900, 1, 1));
-               book3.setPrice(14.99);
-               books.add(book3);
-           };
+           Book book2 = new Book();
+           book2.setAuthor("William Shakespeare");
+           book2.setName("Romeo und Julia");
+           book2.setLanguage("en");
+           book2.setPublishDate(new Date(1597 - 1900, 1, 1));
+           book2.setPrice(8.49);
+           books.add(book2);
 
-           transient
-           private DataModel<Book> booksDataModel;
+           Book book3 = new Book();
+           book3.setAuthor("Stephen King");
+           book3.setName("Es - It");
+           book3.setLanguage("en");
+           book3.setPublishDate(new Date(1986 - 1900, 1, 1));
+           book3.setPrice(14.99);
+           books.add(book3);
+       };
+
+       transient
+       private Book inedit;
+       private DataModel<Book> booksDataModel;
+       private ArrayList<Book> rawBooks;
+       
+       public DataModel<Book> getBooks() {
+    	   if (booksDataModel == null) {
+    		   rawBooks = new ArrayList<Book>();
+    		   rawBooks.addAll(books);
+    		   booksDataModel = new ListDataModel<Book>(rawBooks);
+    	   }
+    	   
+    	   return booksDataModel;
+       }
            
-           public DataModel<Book> getBooks() {
-        	   if (booksDataModel == null)
-        		   booksDataModel = new ListDataModel<Book>(books);
+		public Book getInedit() {
+			return inedit;
+		}
 
-        	   return booksDataModel;
-           }   
+		public void setInedit(Book inedit) {
+			this.inedit = inedit;
+		}
+		
+		public String prepareNewBook() {
+			inedit = new Book();
+			return "create";
+		}
+		
+       public String saveBook() {
+    	   if (!rawBooks.contains(inedit)) {
+    		   rawBooks.add(inedit);
+    		   booksDataModel = new ListDataModel<Book>(rawBooks);
+    	   }
+    	   inedit = null;
+    	   return "index";
+       }
+		
 }
